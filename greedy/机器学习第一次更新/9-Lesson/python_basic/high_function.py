a82fa62d21c3b960a4058dcf434e7ed1a50e4825
bijh <bijh@tsingyun.net>
2020-03-25 10:27:25 +0800

    "lamda"

--- greedy/机器学习第一次更新/9-Lesson/python_basic/high_function.py
+++ greedy/机器学习第一次更新/9-Lesson/python_basic/high_function.py
@@ -1,5 +1,3 @@
-__author__ = "zhou"
-__date__ = "2019-05-11 20:31"
 
 # lambda表达式    又叫匿名函数
 # 格式：   lambda 参数列表: 表达式
@@ -14,25 +12,17 @@ r = f(2)
 
 # map 函数
 map_demo = map(lambda x: x + x, [1, 2, 3, 4])
-# print(map_demo)
-# print(list(map_demo))
-#
-# print(list(map(str, [1, 2, 3, 4])))
+print(map_demo)
+print(list(map_demo))
+print(list(map(str, [1, 2, 3, 4])))
 
 
 # reduce函数
 from functools import reduce
 r = reduce(lambda x, y: x+y, [1, 2, 3, 4], 10)
-# print(r)
-
+print(r)
 
 
 # filter函数
-
 print(list(filter(lambda x: x % 2 == 1, [1, 2, 3, 4, 5, 6, 7, 8])))
-
 print(list(filter(lambda x: x and x.strip(), ["1", "aa", " ", None])))
-
-
-
-
